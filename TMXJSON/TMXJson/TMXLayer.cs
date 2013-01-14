dc51ad189ff7bda69a843277f3cfffe2dcef9dc9
Will McCullough <w.c.mccullough@me.com>
2013-01-14 10:40:33 -0500

    Initial upload

---
+++ TMXJSON/TMXJson/TMXLayer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace TMXJson
+{
+    public class TMXLayer
+    {
+        /// <summary>
+        /// The name of the layer
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// The width of the layer
+        /// </summary>
+        public int Width { get; private set; }
+
+        /// <summary>
+        /// The height of the layer
+        /// </summary>
+        public int Height { get; private set; }
+
+        /// <summary>
+        /// The tile index data for the layer
+        /// </summary>
+        public int[,] Data { get; private set; }
+
+        /// <summary>
+        /// The layer type
+        /// </summary>
+        public TMXLayerType Type { get; private set; }
+
+        /// <summary>
+        /// The opacity of the layer
+        /// </summary>
+        public float Opacity { get; private set; }
+
+        /// <summary>
+        /// The visibility of the layer
+        /// </summary>
+        public bool Visible { get; private set; }
+
+        /// <summary>
+        /// Constructs a new layer with the specified values
+        /// </summary>
+        /// <param name="name">The name of the layer</param>
+        /// <param name="width">The width of the layer</param>
+        /// <param name="height">The height of the layer</param>
+        /// <param name="data">The tile index data for the layer</param>
+        /// <param name="opacity">The opacity for the layer</param>
+        /// <param name="type">The type of layer</param>
+        /// <param name="visible">The visibility for the layer</param>
+        public TMXLayer(string name, int width, int height, int[,] data, float opacity, TMXLayerType type, bool visible)
+        {
+            this.Name = name;
+
+            this.Width = width;
+
+            this.Height = height;
+
+            this.Data = data;
+
+            this.Opacity = opacity;
+
+            this.Type = type;
+
+            this.Visible = visible;
+        }
+
+        /// <summary>
+        /// Sets the opacity to a value between 0.0 and 1.0
+        /// </summary>
+        /// <param name="opacity"></param>
+        public void SetOpacity(float opacity)
+        {
+            if (opacity > 1.0)
+                this.Opacity = 1.0f;
+
+            if (opacity < 0.0)
+                this.Opacity = 0.0f;
+
+            this.Opacity = opacity;
+        }
+
+        /// <summary>
+        /// Sets the layer visibility to the specified value
+        /// </summary>
+        /// <param name="visible"></param>
+        public void SetVisible(bool visible)
+        {
+            this.Visible = visible;
+        }
+
+        /// <summary>
+        /// Toggles the layer visibility
+        /// </summary>
+        public void ToggleVisible()
+        {
+            this.Visible = !this.Visible;
+        }
+
+        /// <summary>
+        /// Returns the tile index at the specified location
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int GetTileIndex(int x, int y)
+        {
+            return Data[x, y];
+        }
+    }
+}
---
+++ TMXJSON/TMXJson/TMXTileset.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMXJson
+{
+    public class TMXTileset
+    {
+        public string Name { get; private set; }
+
+        public int TileWidth { get; private set; }
+
+        public int TileHeight { get; private set; }
+
+        public int ImageWidth { get; private set; }
+
+        public int ImageHeight { get; private set; }
+
+        public string ImagePath { get; private set; }
+
+        public int Margin { get; private set; }
+
+        public int FirstGrid { get; private set; }
+
+        public int Spacing { get; private set; }
+
+        public Dictionary<string, string> Properties { get; private set; }
+
+        public Dictionary<string, string> TileProperties { get; private set; }
+
+        public TMXTile[] Tiles { get; private set; }
+
+        public TMXTileset(string name, int tileWidth, int tileHeight, int imageWidth, int imageHeight, string imagePath, int margin, int firstgid, int spacing, Dictionary<string, string> properties, Dictionary<string, string> tileProperties)
+        {
+            this.Name = name;
+
+            this.TileWidth = tileWidth;
+
+            this.TileHeight = tileHeight;
+
+            this.ImageWidth = imageWidth;
+
+            this.ImageHeight = imageHeight;
+
+            this.ImagePath = imagePath;
+
+            this.Margin = margin;
+
+            this.FirstGrid = firstgid;
+
+            this.Spacing = spacing;
+
+            this.Properties = properties;
+
+            this.TileProperties = tileProperties;
+
+            Tiles = new TMXTile[(imageWidth / tileWidth * imageHeight / tileHeight) + 1];
+
+            int i = 1;
+            Tiles[i] = new TMXTile(0, 0, 0, 0, 0);
+
+            for (int y = 0; y < imageHeight / tileHeight; y++)
+            {
+                for (int x = 0; x < imageWidth / tileWidth; x++)
+                {
+                    Tiles[i] = new TMXTile(i, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+
+                    i++;
+                }
+            }
+        }
+
+        public TMXTile GetTile(int gid)
+        {
+            return Tiles.First(t => t.ID == gid);
+        }
+    }
+}
